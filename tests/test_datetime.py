c163bfab6d345f06182d6931bacace3ece7219ad
Michael Holschbach <michael.holschbach@devops.ncia.nato.int>
2025-03-14 16:52:31 +0100

    Fix failing test by setting the expected timezone

--- tests/test_datetime.py
+++ tests/test_datetime.py
@@ -15,6 +15,7 @@ def test_dt_to_w3cdtf_from_timezone_aware_dt_object():
 
 
 def test_dt_from_w3cdtf_from_string():
+    tz = timezone(timedelta(hours=5, minutes=30))
     dt_stamp = Dt.from_w3cdtf("2023-09-26T15:01:32Z").timestamp()
     assert dt_stamp == datetime(year=2023, month=9, day=26,
-                                hour=20, minute=31, second=32).timestamp()
+                                hour=20, minute=31, second=32, tzinfo=tz).timestamp()
